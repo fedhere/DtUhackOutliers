4886a69a1a678c35786a89dde39436782a785276
fedhere <fedhere@gmail.com>
2018-10-15 23:54:42 -0400

    bug fixes, better var names

--- scripts/sparsify.py
+++ scripts/sparsify.py
@@ -12,7 +12,8 @@ def sporadicSampling(N, indata, square=False):
     np.random.seed(123)
 
     #index of arrays created as uniform sampling of the indeces of each object
-    subSampled = (np.random.rand(indata.shape[0], N) * ind[0][0][0]).astype(int)
+    subSampled = (np.random.rand(indata.shape[0], N) *
+                  len(indata[0][0])).astype(int)
     subSampled.sort(axis=1)
     
     #identify duplicate indeces
@@ -24,25 +25,37 @@ def sporadicSampling(N, indata, square=False):
             subSampled[:,1:][duplicates] += 1
             duplicates = np.diff(subSampled, axis=1) == 0
         
-    return [subSampled[i][a] for i,a in
+    newind = [subSampled[i][a] for i,a in
      enumerate(np.concatenate([np.atleast_2d(
          np.array([True]*duplicates.shape[0])).T, ~duplicates], axis=1))]
+    return newind, indata.shape, \
+        np.array([[indata[i][0][ni], indata[i][1][ni],
+                   indata[i][2][ni]] for i,ni in enumerate(newind)])
 
 if __name__ == '__main__':
     #testing on 5 objects from KeplerSampleFullQ.npy
-    ind = np.load("../data/KeplerSampleFullQ.npy")[:5]
-    ind = np.array(([i[j] for i in ind
-                      for j in range(3)])).reshape(ind.shape[0],3,
-                                                   len(i[0]))
+    kobjects5 = np.load("../data/KeplerSampleFullQ.npy")[:5]
+    kobjects5 = np.array(([k[j] for k in kobjects5
+                      for j in range(3)])).reshape(kobjects5.shape[0], 3,
+                                                   len(k[0]))
     ax = pl.figure().add_subplot(111)
     i=0
-    pl.plot(ind[i][0], ind[i][1])
-    pl.show()
-    
+    pl.errorbar(kobjects5[i][0], kobjects5[i][1], yerr=kobjects5[i][2],
+                fmt='--')
 
-    for i,a in enumerate(sporadicSampling(10, ind, square=True)):
-        print (ind[i][:,a].shape)        
-        ax.errorbar(ind[i][0,a], ind[i][1,a], yerr=ind[i][2,a])
+    
+    print(sporadicSampling(10, kobjects5, square=False)[2])
+    for k,a in enumerate(sporadicSampling(10, kobjects5, square=True)[0]):
+        print ("here", kobjects5[i][:,a].shape)
+
+        ax.errorbar(kobjects5[i][0,a], kobjects5[i][1,a],
+                    yerr=kobjects5[i][2,a], fmt='x')
+
+    for k,a in enumerate(sporadicSampling(10, kobjects5, square=True)[2]):
+        ax.errorbar(a[0], a[1],
+                    yerr=a[2], fmt='*')
+
+        
 
     ax.set_xlabel("time")
     ax.set_ylabel("flux")
